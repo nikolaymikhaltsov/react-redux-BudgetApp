cd6aa1c5d4feca6c848f1c0406351267b545ba46
MMykhaltsov <MMykhaltsov@luxoft.com>
2016-12-15 17:25:38 +0200

    fix value type

--- src/Utils.js
+++ src/Utils.js
@@ -3,10 +3,9 @@ import _ from 'lodash';
 export function getPercentFromSalary(value, salary) {
   if (!salary)
     return '';
-  return (value / salary * 100).toFixed(1);
+  return parseFloat((value / salary * 100).toFixed(1));
 }
 
 export function sortItemsByProperty(items, prop) {
   return _.sortBy(items, prop);
 }
-
